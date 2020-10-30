cf8a092998bc504884d5edd0d4d2ce6318a12b03
pial ebny habib <khanmd.pialebnyhabib@gmail.com>
2020-10-30 19:34:09 +0600

    final

--- src/Components/Home/About/About.js
+++ src/Components/Home/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <div className="container " >
          
             <img className="images" src={image}/>
-            <h1 className="Box">  Hello, <br></br> <br></br>I'm a passionate  Web developer , everyday trying to learn something new and exsitng things so that i can contribute somthing to my country and people. </h1> 
+            <h1 className="Box">  Hello, <br></br> <br></br>I'm a passionate  Web developer , everyday trying to learn something new and excitng things so that i can contribute something to my country and people. </h1> 
             <br></br>
             <div className="resume"><Link  target="blank"  to="https://drive.google.com/file/d/1s7cKVVRJ0XBjyj6_0OEAHHENNaQaxolN/view?usp=sharing "download >  <button type="button" class="btn btn-light">Downlode Resume</button></Link></div>
             
