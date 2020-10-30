3ad4052f6acaaf5a017306573cb64b01b6fc83f0
pial ebny habib <khanmd.pialebnyhabib@gmail.com>
2020-10-30 19:17:50 +0600

    final

--- src/Components/Home/Musics/Musics.js
+++ src/Components/Home/Musics/Musics.js
@@ -7,7 +7,7 @@ const Musics = () => {
                      
                     <div className="col-md-12">
                         <h3 className="third">I'm also a Musician,here are some of my songs .</h3>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/jf9HqrTjYSY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe width="400" height="315" src="https://www.youtube.com/embed/jf9HqrTjYSY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                    <br></br>
                     <div ><button type="button" class="btn btn-dark"><a className="seemore" target="blank" href="https://www.youtube.com/channel/UCryx1gV_ETJmxNKx3p7deNA?sub_confirmation=1">See more</a></button></div>
                     </div>
